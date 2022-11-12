8ebbaea5f2363b36e009f2589a32bd0e77df64fc
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2022-11-12 17:44:14 +0100

    Removed unused code

--- custom_components/helios/const.py
+++ custom_components/helios/const.py
@@ -7,12 +7,3 @@ SPEED_MAX = "max"
 
 SIGNAL_HELIOS_STATE_UPDATE = "helios_state_update"
 SCAN_INTERVAL = datetime.timedelta(seconds=10)
-
-#  VALUE_TO_SPEED = {
-    #  0: SPEED_OFF,
-    #  1: SPEED_LOW,
-    #  2: SPEED_MEDIUM,
-    #  3: SPEED_HIGH,
-    #  4: SPEED_MAX
-#  }
-#  SPEED_TO_VALUE = {v: k for k, v in VALUE_TO_SPEED.items()}
