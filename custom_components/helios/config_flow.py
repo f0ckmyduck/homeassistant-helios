4f25ab2527c26c1dcb972ae8c4d80c5b6badc4c5
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2022-11-13 14:42:38 +0100

    Use mac address for unique id

--- custom_components/helios/config_flow.py
+++ custom_components/helios/config_flow.py
@@ -2,6 +2,7 @@ from homeassistant import config_entries
 import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 from homeassistant.const import CONF_HOST, CONF_NAME
+import homeassistant.helpers.device_registry as dr
 
 import eazyctrl
 
@@ -24,12 +25,25 @@ class DomainConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         if user_input is not None:
-            await self.async_set_unique_id(user_input[CONF_NAME])
-            self._abort_if_unique_id_configured()
 
             try:
                 client = eazyctrl.EazyController(user_input[CONF_HOST])
                 device = client.get_variable("v00000", 30)
+
+                print("Current device is: " + str(device))
+
+                kwl_mac = client.get_variable("v00002", 18, conversion=str)
+
+                if kwl_mac is not None:
+                    device_unique_id = dr.format_mac(kwl_mac)
+
+                    await self.async_set_unique_id(device_unique_id)
+                    self._abort_if_unique_id_configured()
+
+                else:
+                    print("No unique ID!")
+
+
             except:
                 return self.async_show_form(
                     step_id="user",
